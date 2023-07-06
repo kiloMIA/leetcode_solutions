a0c563d36c0fe13df5baaf5c3ca616e00ec98605
kilo <pipianino@protonmail.com>
2023-07-06 12:01:13 +0600

    formating

--- Two Sum II - Input Array Is Sorted /solution.py
+++ Two Sum II - Input Array Is Sorted /solution.py
@@ -3,13 +3,13 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-       left = 0
-       right = len(numbers)-1
+        left = 0
+        right = len(numbers) - 1
 
-       while left < right:
-           if numbers[left]+numbers[right]<target:
-               left += 1
-           elif numbers[left]+numbers[right]>target:
-               right -= 1
-           else:
-               return [left+1,right+1]
+        while left < right:
+            if numbers[left] + numbers[right] < target:
+                left += 1
+            elif numbers[left] + numbers[right] > target:
+                right -= 1
+            else:
+                return [left + 1, right + 1]
